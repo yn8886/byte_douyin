9a4fd303dbefdfc2516dd11dc0194a25586934a7
yn8886 <1219156469@qq.com>
2023-02-05 18:13:21 +0800

    测试解析token

---
+++ middlewares/jwt_test.go
@@ -0,0 +1,12 @@
+package middlewares
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAnalyseToken(t *testing.T) {
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjI0fQ.BHCcYIi2OOuGklleLE8O9m6hInGR8FHeudAGQVIdDR4"
+	userClaims, _ := AnalyseToken(token)
+	fmt.Println(userClaims.UserId)
+}
