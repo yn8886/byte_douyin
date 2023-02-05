62a79ad9559744012ab9767f94d900f6b11f7fbe
yn8886 <1219156469@qq.com>
2023-02-05 18:18:52 +0800

    用户注册接口

---
+++ handlers/user_register_handler.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"go_code/project/byte_douyin/models"
+	"go_code/project/byte_douyin/services"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+type UserRegisterResponse struct {
+	models.CommonResponse
+	*services.RegisterResponse
+}
+
+func UserRegisterHandler(c *gin.Context) {
+	username := c.Query("username")
+	val, _ := c.Get("hash_password")
+	password := val.(string)
+	registerResponse, err := services.GetUserRegisterResponse(username, password)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, UserRegisterResponse{
+			CommonResponse: models.CommonResponse{
+				StatusCode: 500,
+				StatusMsg: err.Error(),
+			},
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, UserRegisterResponse{
+		CommonResponse: models.CommonResponse{StatusCode: 0},
+		RegisterResponse: registerResponse,
+	})
+}
