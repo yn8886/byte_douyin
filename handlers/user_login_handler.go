a8fbb778eb0abcbe25e56b83b03f7a47bf803ccf
yn8886 <1219156469@qq.com>
2023-02-05 18:18:43 +0800

    用户登录接口

---
+++ handlers/user_login_handler.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"go_code/project/byte_douyin/models"
+	"go_code/project/byte_douyin/services"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserLoginResponse struct {
+	models.CommonResponse
+	*services.LoginResponse
+}
+
+func UserLoginHandler(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
+
+	LoginResponse, err := services.GetUserLoginResponse(username, password)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, UserLoginResponse{
+			CommonResponse: models.CommonResponse{
+				StatusCode: 500,
+				StatusMsg: err.Error(),
+			},
+		})
+		return
+	}
+	c.JSON(http.StatusOK, UserLoginResponse{
+		CommonResponse: models.CommonResponse{StatusCode: 0},
+		LoginResponse: LoginResponse,
+	})
+	
+}
