fe8696981781e1686ec6612c9cbe18b772f12865
yn8886 <1219156469@qq.com>
2023-02-06 18:01:24 +0800

    修改注释

--- services/user_login_service.go
+++ services/user_login_service.go
@@ -19,28 +19,28 @@ type LoginRequest struct {
 	token 	 string
 }
 
-
+//查询登录用户，得到user_id和token
 func PostUserLogin(username, password string) (*LoginResponse, error) {
 	return (&LoginRequest{username: username, password: password}).Do()
 }
 
 
 func (r *LoginRequest) Do() (*LoginResponse, error) {
-	//查询用户是否存在
+	//从数据库中查询用户
 	if ! models.UserIsExistByUsername(r.username) {
 		return nil, errors.New("用户名不存在，请先注册")
 	}
-	//查询用户名和密码是否匹配，若匹配则生成token
+	//查询数据库验证用户名和密码，生成token
 	if err := r.QueryLoginData(); err != nil {
 		return nil, err
 	}
-	//返回用户id和权限token
+	//返回user_id和token
 	return r.PackLoginResponse(), nil
 }
 
 func (r *LoginRequest) QueryLoginData() error {
 	userLogin := models.UserLogin{}
-	//查询数据库数值是否一致
+	//验证用户名和密码
 	err := models.CheckNameAndPwd(r.username, r.password, &userLogin)
 	if err != nil {
 		return err
